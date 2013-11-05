a5a94dfd47426cb9813c3c5cca70923a832705f8
Riarius <christianfink79@googlemail.com>
2013-11-05 11:21:13 -0800

    Hello World!

--- src/main.c
+++ src/main.c
@@ -5,12 +5,13 @@
 
 #define MY_UUID { 0x47, 0xD1, 0x63, 0xC6, 0x9B, 0x46, 0x4E, 0x05, 0x88, 0xEF, 0x82, 0x83, 0x91, 0xA0, 0x44, 0x0C }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
+             "Hello World", "Christian Fink",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
 Window window;
+TextLayer helloLayer;
 
 
 void handle_init(AppContextRef ctx) {
@@ -18,6 +19,12 @@ void handle_init(AppContextRef ctx) {
 
   window_init(&window, "Window Name");
   window_stack_push(&window, true /* Animated */);
+	
+	text_layer_init(&helloLayer, GRect(0, 65, 144, 30));
+	text_layer_set_text_alignment(&helloLayer, GTextAlignmentCenter);
+	text_layer_set_text(&helloLayer, "Hello World!");
+	text_layer_set_font(&helloLayer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+	layer_add_child(&window.layer, &helloLayer.layer);
 }
 
 
